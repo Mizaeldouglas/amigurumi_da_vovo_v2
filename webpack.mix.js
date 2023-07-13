ce62f3781fccbe5612c315f52f718a13d7c9d71b
mizaeldouglas <mizaeldouglas2@gmail.com>
2023-07-12 22:28:33 -0300

    modificando package.json e adicionado webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js('resources/js/app.js', 'public/assets/js')
+    .postCss('resources/css/app.css', 'public/assets/css', [
         require('tailwindcss'),
     ])
     .sourceMaps();
